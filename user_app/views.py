270a4e06edc901dabe8b24909a73410bf7ff341c
wangyang <724908809@qq.com>
2018-09-27 10:07:15 +0800

    update login

--- user_app/views.py
+++ user_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
 
 
 # Create your views here.
@@ -10,13 +11,15 @@ def index(request):
 
 def login_action(request):
     if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        user = auth.authenticate(username=username, password=password)
         if username == '' or password == '':
             return render(request, 'index.html', {"error": "账号或密码不能为空！"})
-        if username == 'admin' and password == 'admin':
-            return render(request,'login_action.html')
-        if username != 'admin' or password != 'admin':
-            return render(request, 'index.html', {"error": "账号或密码错误，请重新输入！"})
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'project_manage.html')
+        else:
+            return render(request, 'index.html', {"error": "账号或密码错误！"})
     else:
         return render(request, 'index.html')
